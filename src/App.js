d81f8113cc706d32a8e6f7211470050aab79f414
Shah <shebiraaja@gmail.com>
2022-02-12 13:25:38 +0100

    hecho un post de like y dislike despues del contador

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
 
-  const [count, setCount] = useState(0)
+  const [likes, setLikes] = useState(0)
 
   return (
     <div>
-      <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count+1)}>
-        Click here
-      </button>
+      <img src="https://imagenes.20minutos.es/files/image_656_370/files/fp/uploads/imagenes/2021/09/29/cristiano-ronaldo.r_d.1684-405-1942.jpeg" alt="cr7" />
 
-      <button onClick={() => setCount(count-1)}>
-        Don't click here
-      </button>
+      <p>Likes : {likes}</p>
 
-      <button onClick={() => setCount(count*0)}>
-        Erase
-      </button>
-    </div>
+      <button onClick={() => setLikes(likes + 1)}>Like</button>
+
+      <button onClick={() => setLikes(likes - 1)}>Dislike</button>
+    
+      </div>
   );
 }
 
