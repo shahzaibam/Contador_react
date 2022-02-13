6147baf92ae313aebe318ef6f6d4b362d267c395
Shah <shebiraaja@gmail.com>
2022-02-13 13:56:31 +0100

    exploring useState

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
 
-  const [likes, setLikes] = useState(0)
+  const [likes, setLikes] = useState(false)
   const [comment, setComment] = useState("")
 
   useEffect(() => {
@@ -13,17 +13,21 @@ function App() {
 
   return (
     <div>
-      <img src="https://imagenes.20minutos.es/files/image_656_370/files/fp/uploads/imagenes/2021/09/29/cristiano-ronaldo.r_d.1684-405-1942.jpeg" alt="cr7" />
+
+      {likes === true ?       <div>
+        <img src="https://imagenes.20minutos.es/files/image_656_370/files/fp/uploads/imagenes/2021/09/29/cristiano-ronaldo.r_d.1684-405-1942.jpeg" alt="cr7" />
+      </div> : <div>nothing</div>}
+
 
       <p>Likes : {likes}</p>
 
-      <button onClick={() => setLikes(likes + 1)}>Like</button>
+      <button onClick={() => setLikes(true)}>Like</button>
 
-      <button onClick={() => setLikes(likes === 0 ? likes : likes - 1)}>Dislike</button>
-    
+      <button onClick={() => setLikes(false)}>Dislike</button>
+
       <p>{comment}</p>
       <input onChange={chn => setComment(chn.target.value)} value={comment} />
-      </div>
+    </div>
   );
 }
 
